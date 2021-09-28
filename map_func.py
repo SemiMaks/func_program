9aa52061d8689124c2524bda74dbe404dfb4587b
Maksim <semimaks@mail.ru>
2021-09-28 14:31:14 +0300

    Добавлены программы с примерами работы lambda, map

---
+++ map_func.py
@@ -0,0 +1,12 @@
+# map позволяет преобразовать некую последовательность в другую.
+# отображение(map) позволяет применить специальную функцию для всех
+# элементов последовательности.
+# 'map(функция, последовательность)'
+
+from lambda_func import lambda_function
+
+seq1 = (1, 2, 3, 4, 5)
+seq2 = (6, 7, 8, 9, 10)
+seq3 = (11, 12, 13, 14, 15)
+print(map(lambda_function, seq1, seq2, seq3))
+print(list(map(lambda_function, seq1, seq2, seq3)))
